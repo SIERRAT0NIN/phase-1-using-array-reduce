8688455d61165d403225036124b76e7823feaa91
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-09-16 21:14:23 -0700

    done array reduce

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce((acc, current) => acc + current);
+console.log(totalBatteries);
+
+// const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue);
